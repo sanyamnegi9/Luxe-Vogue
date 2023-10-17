abbca451fb6d0eac0423c58027f3d3f0dec7f7e1
Sanyam <Sanyamnegi@proton.me>
2023-10-17 17:49:40 +0530

    first Commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+
+const Home = () => {
+  return (
+    <>
+     <Navbar/>
+     <h2>Home</h2> 
+     <Footer/>
+    </>
+  );
+};
+
+export default Home;
