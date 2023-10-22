516e016ad088b17772cf0ac37c4ce4b8961792eb
Sanyam <Sanyamnegi@proton.me>
2023-10-22 17:41:15 +0530

    Start Luxe Vogue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-// import Navbar from "./Navbar";
-import NavbarTwo from "./NavbarTwo";
+import Navbar from "./Navbar";
+import Footer from './Footer';
 import { Home, About, Contact } from "./Pages";
 
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
-      <NavbarTwo />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,9 @@
-import Navbar from "../Navbar";
-import Footer from "../Footer";
-
 const Home = () => {
   return (
     <>
-    <div>Home</div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat mollitia obcaecati nisi exercitationem! Impedit magni sequi harum autem veritatis aspernatur? Deserunt consequuntur earum praesentium, natus voluptatum expedita iusto vero, odit nulla doloremque, quisquam amet ipsam labore esse aspernatur? Iste beatae accusamus dolor! Est nemo dolorem perferendis facere dignissimos neque at!</p>
+      <div>Home</div>
     </>
   );
 };
 
-export {Home};
+export { Home };
