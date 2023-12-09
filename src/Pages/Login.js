328fea4b1f13f4e152b3964e9c00bcffb7f8bfaf
Sanyam <Sanyamnegi@proton.me>
2023-12-09 11:48:53 +0530

    login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserStatusContext } from "../contexts/UserStatusContext";
 
 const Login = () => {
@@ -17,29 +17,45 @@ const Login = () => {
   };
 
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <h5>login</h5>
-        <div>
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button type="submit">Submit</button>
+    <section className="flex items-center justify-center h-[calc(100vh-7.4rem)]">
+      <form
+        onSubmit={handleSubmit}
+        className=" bg-background-sec flex flex-col items-center justify-center h-[20rem] w-full px-2 sm:px-0 sm:w-[25rem]"
+      >
+        <h3 className=" text-[1.3rem] mb-4">login</h3>
+        <div className="flex flex-col gap-6 items-center">
+          <div className="input-box">
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="input sm:w-[16rem]"
+              required
+            />
+            <label id="name" className="label">
+              Name
+            </label>
+          </div>
+          <div className="input-box">
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input sm:w-[16rem]"
+              required
+            />
+            <label id="email" className="label">
+              Email
+            </label>
+          </div>
+          <button type="submit" className="btn primary-btn">
+            Submit
+          </button>
+          <h3 className=" text-[.8rem] text-center">
+            Don't have an account <Link to="/signup" className="underline text-sec-text hover:text-text">Sign Up</Link>
+          </h3>
         </div>
       </form>
     </section>
